f91487a16682623354e7a2e0684d2c5ced713fb9
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-06-12 17:55:49 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// IMPORT route and our components
+// component libraries
+import { Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
